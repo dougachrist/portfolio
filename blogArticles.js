907da0b9cd65cc3e22faaff5fce418062f648279
dougachrist <dougachrist@gmail.com>
2016-07-13 10:14:21 -0700

    push blog post using array

---
+++ blogArticles.js
@@ -0,0 +1,23 @@
+blogArticles = [
+  {
+    title: 'Hawaii Vacay',
+    author: 'Doug Christ',
+    publishedDate: '2013-12-31',
+    img: 'images/hawaii.jpg',
+    body: '<p>If this is showing up, then that is a win!</p>'
+  },
+  {
+    title: 'DreamForce',
+    author: 'Doug Christ',
+    publishedDate: '2014-11-15',
+    img: 'images/dreamforce.jpg',
+    body: '<p>DreamForce ROCKS!</p>'
+  },
+  {
+    title: 'From the Feet UP',
+    author: 'Doug Christ',
+    publishedDate: '2015-2-15',
+    img: 'images/shoes.jpg',
+    body: '<p>This is just some blog text</p>'
+  }
+];
