4b1b58bece54532139c778a86b4196603ab63c01
dougachrist <dougachrist@gmail.com>
2016-07-14 00:04:16 -0700

    add linksand content for rest of nav items

--- app.js
+++ app.js
@@ -1,43 +1,77 @@
 $(document).ready( function() {
+  loadHome();
   $('nav').click(function() {
     $(this).toggleClass('activeState');
     $('.starfish img').slideToggle(350);
   });
 });
 
-var allArticles = [];
+function loadProjects() {
+  var allArticles = [];
 
-function BuildArticle (blogPost) {
-  this.title = blogPost.title;
-  this.img = blogPost.img;
-  this.narrativeHTML = blogPost.body;
-  this.publishedDate = blogPost.publishedDate;
-  this.author = blogPost.author;
-}
+  function BuildArticle (blogPost) {
+    this.title = blogPost.title;
+    this.img = blogPost.img;
+    this.narrativeHTML = blogPost.body;
+    this.publishedDate = blogPost.publishedDate;
+    this.author = blogPost.author;
+  }
 
-BuildArticle.prototype.toHtml = function () { //defines ".toHtml" to "Article" just this one time; Reference.
-  var $newArticle = $('section.template').clone(); //"var $whatever" will contain a jQuery object.
-  $newArticle.removeClass();
-  $newArticle.find('time[pubdate]').attr('title', this.publishedDate);
-  $newArticle.find('h2').text(this.author);
-  $newArticle.find('time').html('published about ' + parseInt((new Date() - new Date(this.publishedDate))/60/60/24/1000) + ' days ago');
-  $newArticle.find('h1').text(this.title);
-  $newArticle.find('img').attr('src',this.img);
-  $newArticle.find('div.blogText').html(this.narrativeHTML);
+  BuildArticle.prototype.toHtml = function () { //defines ".toHtml" to "Article" just this one time; Reference.
+    var $newArticle = $('section.template').clone(); //"var $whatever" will contain a jQuery object.
+    $newArticle.removeClass();
+    $newArticle.find('time[pubdate]').attr('title', this.publishedDate);
+    $newArticle.find('h2').text(this.author);
+    $newArticle.find('time').html('published about ' + parseInt((new Date() - new Date(this.publishedDate))/60/60/24/1000) + ' days ago');
+    $newArticle.find('h1').text(this.title);
+    $newArticle.find('img').attr('src',this.img);
+    $newArticle.find('div.blogText').html(this.narrativeHTML);
 
-  return $newArticle;
-};
+    return $newArticle;
+  };
 
-blogArticles.sort(function(a,b) { //check about .sort() on MDN
-  return (new Date(b.publishedDate)) - (new Date(a.publishedDate));
-});
+  blogArticles.sort(function(a,b) { //check about .sort() on MDN
+    return (new Date(b.publishedDate)) - (new Date(a.publishedDate));
+  });
 
-blogArticles.forEach(function(ele) { //"ele" refers to each of the elements in local data; Placeholder.
-  allArticles.push(new BuildArticle(ele));
-});
+  blogArticles.forEach(function(ele) { //"ele" refers to each of the elements in local data; Placeholder.
+    allArticles.push(new BuildArticle(ele));
+  });
 
-function loadProjects () {
   allArticles.forEach(function(a) {
     $('main').append(a.toHtml());
   });
+}
+
+function loadHome() {
+  var $section = $('<section></section>');
+  var $img = $('<img>');
+  $img.attr('src','images/dreamforce.jpg');
+  var $h1 = $('<h1></h1>');
+  $h1.text('Welcome to my Professional Portfolio');
+  $section.append($h1);
+  $section.append($img);
+  $('main').append($section);
+}
+
+function loadBio() {
+  var $section = $('<section></section>');
+  var $img = $('<img>');
+  $img.attr('src','images/cloud.jpg');
+  var $h1 = $('<h1></h1>');
+  $h1.text('About Me');
+  $section.append($h1);
+  $section.append($img);
+  $('main').append($section);
+}
+
+function loadContacts() {
+  var $section = $('<section></section>');
+  var $img = $('<img>');
+  $img.attr('src','images/starfish.jpg');
+  var $h1 = $('<h1></h1>');
+  $h1.text('Please Contact Me');
+  $section.append($h1);
+  $section.append($img);
+  $('main').append($section);
 }
--- home.js
+++ home.js
@@ -1,11 +1,28 @@
 $(document).ready( function() {
   $('li[data-content=home]').on('click',function() {
     $('section').not('.template').remove();
+    loadHome();
   });
 });
 
 $(document).ready( function() {
   $('li[data-content=projects]').on('click',function() {
+    $('section').not('.template').remove();
     loadProjects();
+  });
+});
+
+
+$(document).ready( function() {
+  $('li[data-content=contact]').on('click',function() {
+    $('section').not('.template').remove();
+    loadContacts();
+  });
+});
+
+$(document).ready( function() {
+  $('li[data-content=bio]').on('click',function() {
+    $('section').not('.template').remove();
+    loadBio();
   });
 });
